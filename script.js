fa936aa8499cb186ffc4cd1d455f708529f63f23
Romain Simonet <romain.simonet@epitech.eu>
2025-09-19 10:49:47 +0200

    Portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.open-modal').forEach(card => {
+  card.addEventListener('click', () => {
+    const modalId = card.getAttribute('data-modal');
+    document.getElementById(modalId).style.display = 'block';
+  });
+});
+
+document.querySelectorAll('.close').forEach(closeBtn => {
+  closeBtn.addEventListener('click', () => {
+    closeBtn.closest('.modal').style.display = 'none';
+  });
+});
+
+window.addEventListener('click', e => {
+  if (e.target.classList.contains('modal')) {
+    e.target.style.display = 'none';
+  }
+});
